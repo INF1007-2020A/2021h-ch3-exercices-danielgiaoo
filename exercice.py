17fc8f6b48d6f6cadb009ec92dea75668a35fbfd
Daniel Giao <70482146+danielgiaoo@users.noreply.github.com>
2021-01-26 14:37:56 -0500

    Mise a jour exo 3

--- exercice.py
+++ exercice.py
@@ -20,7 +20,7 @@ def average(a: float, b: float, c: float) -> float:
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     degres = angle_degs + (angle_mins / 60) + (angle_secs / 3600)
     radian = math.radians(degres)
-    return angle_degs*(math.pi/180)+angle_mins+angle_secs
+    return radian
 
 
 def to_degrees(angle_rads: float) -> tuple:
